32ce1b95ebd38aea4aaabc214acee026875dc0fd
Anastasia Serenko <orangesun94@gmail.com>
2022-05-07 22:01:30 +0700

    initial version

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static bool getCoordinates(string nameVector, ref int[] coordinates)
+        {
+            Console.WriteLine($"Enter coordinate of {nameVector}:");
+            var exp = Console.ReadLine();
+            string[] strCoord = exp.Split(' ');
+
+            if (strCoord.Length == 3)
+            {
+                for (int i = 0; i < strCoord.Length; i++)
+                {
+                    if (!int.TryParse(strCoord[i], out coordinates[i]))
+                        return false;
+                }
+                return true;
+            }
+            else
+                return false;                
+        }
+
+        static void Main(string[] args)
+        {
+            Vector v1, v2;
+            int[] CoordVect1 = { 0, 0, 0 };
+            int[] CoordVect2 = { 0, 0, 0 };
+
+             Console.WriteLine("Enter coordinates {x, y, z} of vectors\n" +
+                 "using space to separate them,\n" +
+                 "for example, 4 5 6:\n");
+
+             if (getCoordinates("vector1", ref CoordVect1) &&
+                 getCoordinates("vector2", ref CoordVect2))
+             {
+                 v1 = new Vector(CoordVect1);
+                 v2 = new Vector(CoordVect2);                
+             }            
+             else 
+            {             
+                 v1 = new Vector(1, 2, 3);
+                 v2 = new Vector(4, 5, 6);
+                 Console.WriteLine("INCORRECT input coordinates!\n" +
+                     "Coordinates was setting with default value");
+             }
+
+             Console.WriteLine($"\nSo, given:\n" +
+                 $"vector1 = {v1},\n" +
+                 $"vector2 = {v2}\n");
+
+             Console.WriteLine("Length of vectors is");
+             Console.WriteLine($"|vector1| = {v1.GetLength()}");
+             Console.WriteLine($"|vector2| = {v2.GetLength()}\n");
+
+             Console.WriteLine("Scalar multiplier of vectors is");
+             Console.WriteLine($"(vector1, vector2) = {Vector.ScalarMultiplier(v1, v2)}\n");
+
+             Console.WriteLine("Vector multiplier of vectors is");
+             Console.WriteLine($"[vector1, vector2] = {(v1 * v2)}\n");
+
+             Console.WriteLine("Cosin of the angle between vectors is");
+             Console.WriteLine($"cos(a) = {Vector.GetAngle(v1, v2)}\n");
+
+             Console.WriteLine("Summation of vectors is");
+             Console.WriteLine($"vector1 + vector2 = {(v1 + v2)}\n");
+
+             Console.WriteLine("Subtraction of vectors is");
+             Console.WriteLine($"vector1 - vector2 = {(v1 - v2)}");
+
+            Console.ReadKey();            
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Vector.cs
@@ -0,0 +1,118 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Vector
+    {    
+        private int _x, _y, _z;     
+        
+        #region Constructors    
+        public Vector(int x, int y, int z) {
+            _x = x;
+            _y = y;
+            _z = z;
+        }
+        
+        public Vector(int[] coordinates) {
+            if (coordinates.Length == 3) {             
+                _x = coordinates[0];
+                _y = coordinates[1];
+                _z = coordinates[2];
+            }
+            else{             
+                _x = 0;
+                _y = 0;
+                _z = 0;
+            }
+        }       
+        #endregion
+
+        #region SetGetMethods   
+        public int X
+        {
+            get { return _x; }
+            set { _x = value; }
+        }
+
+        public int Y
+        {
+            get { return _y; }
+            set { _y = value; }
+        }
+
+        public int Z
+        {
+            get { return _z; }
+            set { _z = value; }
+        }
+        #endregion
+
+        #region UnstaticMethods   
+        public double GetLength() {
+            return Math.Sqrt(Math.Pow(_x, 2) +
+                Math.Pow(_y, 2) +
+                Math.Pow(_z, 2));
+        }
+
+        public override string ToString() {
+            return $"{{{_x}; {_y}; {_z}}}";
+        }
+        #endregion
+
+        #region StaticMethods 
+
+        public static int ScalarMultiplier(Vector vector1, Vector vector2)
+        {
+            return (vector1._x * vector2._x +
+                vector1._y * vector2._y +
+                vector1._z * vector2._z);
+        }
+
+        public static double GetAngle(Vector vector1, Vector vector2)
+        {
+            return (ScalarMultiplier(vector1, vector2) /
+                (vector1.GetLength() * vector2.GetLength()));
+        }
+
+        public static Vector VectorMultiplier(Vector vector1, Vector vector2) {
+            int x, y, z;
+            x = vector1._y * vector2._z - vector1._z * vector2._y;
+            y = vector1._z * vector2._x - vector1._x * vector2._z;
+            z = vector1._x * vector2._y - vector1._y * vector2._x;
+            Vector rez = new Vector(x, y, z);
+            return rez;
+        }
+
+        public static Vector Summation(Vector vector1, Vector vector2)
+        {
+            Vector rez = new Vector((vector1._x + vector2._x),
+                (vector1._y + vector2._y),
+                (vector1._z + vector2._z));
+            return rez;
+        }
+
+        public static Vector Subtraction(Vector vector1, Vector vector2)
+        {
+            Vector rez = new Vector((vector1._x - vector2._x),
+                (vector1._y - vector2._y),
+                (vector1._z - vector2._z));
+            return rez;
+        }
+        #endregion
+
+        #region OverloadOperators    
+
+        public static Vector operator *(Vector vector1, Vector vector2)
+        {
+            return VectorMultiplier(vector1, vector2);
+        }
+        public static Vector operator +(Vector vector1, Vector vector2) {
+            return Summation(vector1, vector2);
+        }       
+        
+        public static Vector operator -(Vector vector1, Vector vector2) {
+            return Subtraction(vector1, vector2);
+        }            
+        #endregion
+    }
+}
